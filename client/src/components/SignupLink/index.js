f1f442c9524f497f26dd140900ef82d9d6ee05d4
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-17 15:32:06 -0500

    added template signup component with Auth0 integration to manage user register on the page

--- client/src/components/SignupLink/index.js
+++ client/src/components/SignupLink/index.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
-import { Nav, OverlayTrigger, Tooltip } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Nav, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
 
+// added template signup component with Auth0 integration to manage user register on the page
 const SignupLink = () => {
-  const { loginWithRedirect } = useAuth0();
-  return (
-    <OverlayTrigger
-      placement="bottom"
-      overlay={<Tooltip id="tooltip-signup">Sign Up</Tooltip>}
-    >
-      <Nav.Link
-        onClick={() => loginWithRedirect({ screen_hint: 'signup' })}
-      >
-        <FontAwesomeIcon icon={faUserPlus} size="lg" />
-      </Nav.Link>
-    </OverlayTrigger>
-  );
+	const { loginWithRedirect } = useAuth0();
+	return (
+		<OverlayTrigger
+			placement="bottom"
+			overlay={<Tooltip id="tooltip-signup">Sign Up</Tooltip>}
+		>
+			<Nav.Link onClick={() => loginWithRedirect({ screen_hint: "signup" })}>
+				<FontAwesomeIcon icon={faUserPlus} size="lg" />
+			</Nav.Link>
+		</OverlayTrigger>
+	);
 };
 
 export default SignupLink;
