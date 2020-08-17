89223a4824b8a0df6ddb32de310e9004fad14905
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-17 16:45:59 -0500

    example get all route

--- controllers/api/example.js
+++ controllers/api/example.js
@@ -7,3 +7,16 @@ const getEmail = (token) => {
 	decoded = jwt_decode(token);
 	return decoded["https://url/email"];
 };
+
+router.get("/", function (req, res) {
+	let email = getEmail(req.headers.authorization);
+	db.Example.findAll({
+		where: {
+			email: email,
+		},
+	}).then(function (dbAllExample) {
+		res.json(dbAllExample);
+	});
+});
+
+module.exports = router;
