bdeb9a53279288f0d81b65f418babcd7f680c33e
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-17 16:55:27 -0500

    added dependecies with descriptive comments

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
+// added (express npm) =>  Minimalist web framework for node.
 const express = require("express");
-const routes = require("./controllers");
-const app = express();
+// added (morgan npm) =>  HTTP request logger middleware for node.js
 const morgan = require("morgan");
+// added (morgan npm) =>  HTTP request logger middleware for node.js
+const logger = require("morgan");
+// added (mongoose npm) => object modeling tool designed to work in an asynchronous environment in case we need it
+const mongoose = require("mongoose");
+// added required controller to the server
+const routes = require("./controllers");
+
+// created the App
+const app = express();
