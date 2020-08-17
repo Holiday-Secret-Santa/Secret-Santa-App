063142b482f9c105e3149e9e224dd232d4370a03
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-17 16:45:19 -0500

    added example polaceholder function

--- controllers/api/example.js
+++ controllers/api/example.js
@@ -1,3 +1,9 @@
 const router = require("express").Router();
 var db = require("../../models/sql");
 const jwt_decode = require("jwt-decode");
+
+// example auth0 get Email function
+const getEmail = (token) => {
+	decoded = jwt_decode(token);
+	return decoded["https://url/email"];
+};
