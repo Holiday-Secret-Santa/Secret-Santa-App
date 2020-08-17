9f0f08a7bc101070645330ca3429433cd7875ce4
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-17 16:42:50 -0500

    If no API routes are hit, send the React app will display the homepage

--- controllers/index.js
+++ controllers/index.js
@@ -4,3 +4,8 @@ const apiRoutes = require("./api");
 
 // API Routes will start '/api'
 router.use("/api", apiRoutes);
+
+// If no API routes are hit, send the React app will display the homepage
+router.use(function (req, res) {
+	res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
