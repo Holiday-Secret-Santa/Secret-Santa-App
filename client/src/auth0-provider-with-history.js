9ff7a1db291abd7877f39a24652fa183320ab39f
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-17 15:53:27 -0500

    added Auth0ProviderWithHistory from Auth0 to manage authentication

--- client/src/auth0-provider-with-history.js
+++ client/src/auth0-provider-with-history.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+// added from Auth0 Auth0ProviderWithHistory to handle the authentication
 const Auth0ProviderWithHistory = ({ children }) => {
 	const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 	const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
