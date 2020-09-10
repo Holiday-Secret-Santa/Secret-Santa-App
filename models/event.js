b4a468ddaaf3c7560d02e85d810f56d841b20f0f
Yadira Tamarit <ystamaritq@gmail.com>
2020-09-10 12:07:40 -0500

    changed prop into objects to clean up code

--- models/event.js
+++ models/event.js
@@ -1,61 +1,66 @@
+const dateColumn = {
+	type: DataTypes.DATE,
+	allowNull: false,
+	validate: {
+		notNull: {
+			msg: "Must to enter an Date",
+		},
+		isDate: true,
+	},
+};
+const descriptionColumn = {
+	type: DataTypes.TEXT,
+	allowNull: false,
+};
+const startTimeColumn = {
+	type: DataTypes.STRING,
+	allowNull: false,
+	validate: {
+		notNull: {
+			msg: "Please enter the start time",
+		},
+	},
+};
+const endTimeColumn = {
+	type: DataTypes.STRING,
+	allowNull: false,
+	validate: {
+		notNull: {
+			msg: "Please enter the end time",
+		},
+	},
+};
+const locationColumn = {
+	type: DataTypes.STRING,
+	allowNull: false,
+	validate: {
+		notNull: {
+			msg: "Please enter the location",
+		},
+	},
+};
+const plannerEmailColumn = {
+	type: DataTypes.STRING,
+	allowNull: false,
+	validate: {
+		notNull: {
+			msg: "Please enter an email",
+		},
+	},
+	validate: {
+		isEmail: {
+			msg: "Please enter a valid email format: example@gmail.com ",
+		},
+	},
+};
 module.exports = function (sequelize, DataTypes) {
 	var Event = sequelize.define("Event", {
-		date: {
-			type: DataTypes.DATE,
-			allowNull: false,
-			validate: {
-				notNull: {
-					msg: "Must to enter an Date",
-				},
-				isDate: true,
-			},
-		},
-		description: {
-			type: DataTypes.TEXT,
-			allowNull: false,
-		},
-		start_time: {
-			type: DataTypes.STRING,
-			allowNull: false,
-			validate: {
-				notNull: {
-					msg: "Please enter the start time",
-				},
-			},
-		},
-		end_time: {
-			type: DataTypes.STRING,
-			allowNull: false,
-			validate: {
-				notNull: {
-					msg: "Please enter the end time",
-				},
-			},
-		},
-		location: {
-			type: DataTypes.STRING,
-			allowNull: false,
-			validate: {
-				notNull: {
-					msg: "Please enterr the location",
-				},
-			},
-		},
-		planner_email: {
-			type: DataTypes.STRING,
-			allowNull: false,
-			validate: {
-				notNull: {
-					msg: "Please enter an email",
-				},
-			},
-			validate: {
-				isEmail: {
-					msg: "Please enter a valid email format: example@gmail.com ",
-				},
-			},
-		},
+		date: dateField,
+		description: descriptionColumn,
+		start_time: startTimeColumn,
+		end_time: endTimeColumn,
+		location: locationColumn,
+		planner_email: plannerEmailColumn,
 	});
-
 	return Event;
 };
