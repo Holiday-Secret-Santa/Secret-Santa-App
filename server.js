a5ee4c0460ec809b4cd55d8cf8d431d919017620
Yadira Tamarit <ystamaritq@gmail.com>
2020-09-10 11:25:15 -0500

    added sequelize connection to server js

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const routes = require("./controllers");
 const app = express();
 // loads environment variables from a .env file into process.env.
 require("dotenv").config();
+// require models
+var db = require("./models");
 
 const PORT = process.env.PORT || 3001;
 const appOrigin = process.env.APP_ORIGIN;
@@ -19,12 +21,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+	app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
 
 // Start the API server
-app.listen(PORT, () => {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+// Sequelize connection
+db.sequelize.sync({ force: false }).then(() => {
+	app.listen(PORT, () => {
+		console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+	});
 });
