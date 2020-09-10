432d18403bedd6057ac5d41ec61b8229867fdfde
Yadira Tamarit <ystamaritq@gmail.com>
2020-09-09 21:51:38 -0500

    added private routes to App to test the pages

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,10 @@ import { useAuth0 } from "@auth0/auth0-react";
 import PrivateRoute from "./components/PrivateRoute";
 
 import EventsPage from "./pages/Events/Events";
+import CreateEvent from "./pages/CreateEvent/CreateEvent";
+import AcceptEvent from "./pages/AcceptEvent/AcceptEvent";
+import OrganizerEvent from "./pages/OrganizerEvent/OrganizerEvent";
+import ParticipantEvent from "./pages/ParticipantEvent/ParticipantEvent";
 import Loading from "./components/Loading";
 
 const App = () => {
@@ -20,6 +24,14 @@ const App = () => {
 		<Router>
 			{/* no private routes must be place it inside Route*/}
 			<PrivateRoute exact path="/" component={EventsPage} />
+			<PrivateRoute exact path="/events/create" component={CreateEvent} />
+			<PrivateRoute exact path="/events/accept" component={AcceptEvent} />
+			<PrivateRoute exact path="/events/organizer" component={OrganizerEvent} />
+			<PrivateRoute
+				exact
+				path="/events/participant"
+				component={ParticipantEvent}
+			/>
 			{/* private routes must be place it inside PrivateRoute */}
 			<PrivateRoute exact path="/events" component={NavigationBar} />
 		</Router>
