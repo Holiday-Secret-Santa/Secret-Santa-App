28b2f48730422d3a378664f4d07061f3c7d66dbd
Yadira Tamarit <ystamaritq@gmail.com>
2020-09-09 20:45:13 -0500

    updated home page to events dashboard in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,29 @@
 import React from "react";
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import NavigationBar from "./components/NavigationBar";
 // added useAuth0 from @auth0/auth0-react" to handle the routes
 import { useAuth0 } from "@auth0/auth0-react";
 import PrivateRoute from "./components/PrivateRoute";
 
-import HomePage from "./pages/Home";
+import EventsPage from "./pages/Events/Events";
 import Loading from "./components/Loading";
 
 const App = () => {
-  const { isLoading } = useAuth0();
+	const { isLoading } = useAuth0();
 
-  if (isLoading) {
-    return <Loading />;
-  }
+	if (isLoading) {
+		return <Loading />;
+	}
 
-  // placeholder app router with Auth0 integration and external home page that will be replace it by the first wireframe
-  return (
-    <Router>
-      {/* no private routes must be place it inside Route*/}
-      <PrivateRoute exact path="/" component={HomePage} />
-      {/* private routes must be place it inside PrivateRoute */}
-      <PrivateRoute exact path="/events" component={NavigationBar} />
-    </Router>
-  );
+	// placeholder app router with Auth0 integration and external home page that will be replace it by the first wireframe
+	return (
+		<Router>
+			{/* no private routes must be place it inside Route*/}
+			<PrivateRoute exact path="/" component={EventsPage} />
+			{/* private routes must be place it inside PrivateRoute */}
+			<PrivateRoute exact path="/events" component={NavigationBar} />
+		</Router>
+	);
 };
 
 export default App;
