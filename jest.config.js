c93c753cede07ff42ade8ada2fe25a6393cc7c56
Yadira Tamarit <ystamaritq@gmail.com>
2020-09-16 12:43:12 -0500

    Adds jest image mock

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-    // Do not delete:
-    // Jest needs this in order to parse & stub css files. Required by module jest-transform-css.
-    transform: {
-        "^.+\\.js$": "babel-jest",
-        ".+\\.(css|styl|less|sass|scss)$": "jest-transform-css"
-    }
+	// Do not delete:
+	// Jest needs this in order to parse & stub css files. Required by module jest-transform-css.
+	transform: {
+		"^.+\\.js$": "babel-jest",
+		".+\\.(css|styl|less|sass|scss)$": "jest-transform-css",
+		".+\\.(jpg|jpeg|png|gif|webp|svg)$": "jest-transform-file",
+	},
 };
