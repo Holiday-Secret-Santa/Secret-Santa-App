ba04b596da9592b023576c88e20803ebcf1c8c0e
Yadira Tamarit <ystamaritq@gmail.com>
2020-09-17 21:04:58 -0500

    added basic tested grip to participant page

--- client/src/pages/ParticipantEvent/ParticipantEvent.js
+++ client/src/pages/ParticipantEvent/ParticipantEvent.js
@@ -1,8 +1,33 @@
 import React from "react";
+import { Row, Col } from "antd";
 import "./style.css";
+import "../../components/TableComp/TableComp";
+import TableComp from "../../components/TableComp/TableComp";
+import CardComp from "../../components/CardComp";
 
 const ParticipantEvent = () => {
-	return <h1>Participant Event Page</h1>;
+	return (
+		<>
+			<Row style={{ marginTop: "30px" }}>
+				<Col span={2} offset={2}></Col>
+				<Col span={4}>
+					<CardComp />
+				</Col>
+				<Col span={12}>
+					<TableComp />
+				</Col>
+				<Col span={4} offset={4}></Col>
+			</Row>
+			<Row>
+				<Col span={2} offset={2}></Col>
+				<Col span={16}>
+					{" "}
+					<TableComp />
+				</Col>
+				<Col span={4} offset={4}></Col>
+			</Row>
+		</>
+	);
 };
 
 export default ParticipantEvent;
