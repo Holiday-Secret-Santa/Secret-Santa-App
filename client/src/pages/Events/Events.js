9c304976c439bba2d0d71996b9eb77deff242231
KEDuran <krystal.e.garcia@gmail.com>
2020-09-18 09:55:09 -0500

    fixed responsiveness

--- client/src/pages/Events/Events.js
+++ client/src/pages/Events/Events.js
@@ -1,46 +1,50 @@
 import React from "react";
-import { Row, Col, Button, Layout } from "antd";
+import { Row, Col, Button, Layout, Space } from "antd";
 import CardComp from "../../components/CardComp";
 import { PlusCircleOutlined } from "@ant-design/icons";
-// import "antd/dist/antd.css";
 import "./style.css";
 
-const { Content, Footer } = Layout;
+const { Content } = Layout;
 
 const EventsPage = () => {
 	return (
-		<>
-			<Layout>
-				<Content className="white">
-					<Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-						<Col className="gutter-row">
-							<h1>Events Page</h1>
-						</Col>
-					</Row>
-					<Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-						<Col className="gutter-row">
+		<div>
+			<Content className="white">
+				<Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+					<Col className="gutter-row">
+						<h1>Events Page</h1>
+					</Col>
+				</Row>
+				<Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+					<Col className="gutter-row">
+						<Space className="space-top">
 							<CardComp description="Frosty the Snow Man" />
-						</Col>
-						<Col className="gutter-row">
+						</Space>
+					</Col>
+					<Col className="gutter-row">
+						<Space className="space-top">
 							<CardComp description="Feeling the Holiday Spirit" />
-						</Col>
-					</Row>
-					<Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-						<Col className="gutter-row">
-							<Button
-								type="primary"
-								shape="round"
-								icon={<PlusCircleOutlined />}
-								size="large"
-								style={{ backgroundColor: "#2c6e49", borderColor: "#2c6e49" }}
-							>
-								Create New Event
-							</Button>
-						</Col>
-					</Row>
-				</Content>
-			</Layout>
-		</>
+						</Space>
+					</Col>
+				</Row>
+				<Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} className="space-top">
+					<Col className="gutter-row">
+						<Button
+							type="primary"
+							shape="round"
+							icon={<PlusCircleOutlined />}
+							size="large"
+							style={{
+								backgroundColor: "#2c6e49",
+								borderColor: "#2c6e49",
+							}}
+						>
+							Create New Event
+						</Button>
+					</Col>
+				</Row>
+			</Content>
+		</div>
 	);
 };
 
