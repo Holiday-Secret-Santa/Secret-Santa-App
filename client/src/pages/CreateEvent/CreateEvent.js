7a61bda96f6df4ac7cddd974051e273257d4e6f0
JDMartinez1531 <63159799+JDMartinez1531@users.noreply.github.com>
2020-09-17 22:07:22 -0500

    commented transform ignore pattern

--- client/src/pages/CreateEvent/CreateEvent.js
+++ client/src/pages/CreateEvent/CreateEvent.js
@@ -1,5 +1,5 @@
 import React from "react";
-import EventForm from "../../components/EventForm";
+import EventForm from "../../components/EventForm/index";
 import "./style.css";
 
 const CreateEvent = () => {
--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-    // Do not delete:
-    // Jest needs this in order to parse & stub css files. Required by module jest-transform-css.
-    transform: {
-        "^.+\\.js$": "babel-jest",
-        ".+\\.(css|styl|less|sass|scss)$": "jest-transform-css"
-    }
+  // Do not delete:
+  // Jest needs this in order to parse & stub css files. Required by module jest-transform-css.
+  transform: {
+    "^.+\\.js$": "babel-jest",
+    ".+\\.(css|styl|less|sass|scss)$": "jest-transform-css"
+  },
+  // transformIgnorePatterns: [
+  //   "node_modules/(?!(antd/dist/antd.css)/)"
+  // ]
 };
