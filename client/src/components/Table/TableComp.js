d523db3db5669aa265acd817cedd2fb42b6fcd17
Yadira Tamarit <ystamaritq@gmail.com>
2020-09-18 14:09:32 -0500

    table pagination fixed

--- client/src/components/Table/TableComp.js
+++ client/src/components/Table/TableComp.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { Table } from "antd";
+import { Table, Pagination } from "antd";
 
 const TableComp = ({ dataSource, columns }) => {
-	return <Table dataSource={dataSource} columns={columns} />;
+	return (
+		<Table
+			dataSource={dataSource}
+			columns={columns}
+			pagination={{ pageSize: 5 }}
+		/>
+	);
 };
 
 export default TableComp;
