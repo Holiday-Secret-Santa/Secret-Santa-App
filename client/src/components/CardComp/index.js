a1a5429edb55e58871d9232a9bf53605e695d587
KEDuran <krystal.e.garcia@gmail.com>
2020-09-17 20:37:28 -0500

    added responsiveness

--- client/src/components/CardComp/index.js
+++ client/src/components/CardComp/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card, Tooltip } from "antd";
 import { SettingFilled, GiftOutlined } from "@ant-design/icons";
+import "./style.css";
 
 // placeholder parameters for API data
 const exchangeCard = ({
@@ -15,11 +16,13 @@ const exchangeCard = ({
 	<Card
 		title={description}
 		actions={[
-            // Icon buttons for future use
+			// Icon buttons for future use
 			<span>
 				<Tooltip title="Manage Your Gift Exchange">
 					<SettingFilled />
 				</Tooltip>
+			</span>,
+			<span>
 				<Tooltip title="Add Gifts for Secret Santa">
 					<GiftOutlined />
 				</Tooltip>
--- client/src/pages/Events/Events.js
+++ client/src/pages/Events/Events.js
@@ -1,21 +1,27 @@
 import React from "react";
-import { Row, Col } from "antd";
+import { Row, Col, Container } from "antd";
 import CardComp from "../../components/CardComp";
 import "./style.css";
 
 const EventsPage = () => {
 	return (
-		<div>
-			<Row>
-				<h1>Events Page</h1>
+		<div className="white">
+			<Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+				<Col xs={1}></Col>
+				<Col xs={22}>
+					<h1>Events Page</h1>
+				</Col>
+				<Col xs={1}></Col>
 			</Row>
-			<Row>
-				<Col span={12}>
-					<CardComp />
+			<Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+				<Col xs={1}></Col>
+				<Col xs={11}>
+					<CardComp description="Frosty the Snow Man" />
 				</Col>
-				<Col span={12}>
-					<CardComp />
+				<Col xs={11}>
+					<CardComp description="Feeling the Holiday Spirit" />
 				</Col>
+				<Col xs={1}></Col>
 			</Row>
 		</div>
 	);
