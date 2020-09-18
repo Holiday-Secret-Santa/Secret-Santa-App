778052a6046d4976cbbd4b41041c3a5d51c0deb7
Yadira Tamarit <ystamaritq@gmail.com>
2020-09-18 17:49:10 -0500

    Setup jest to mock window.matchMedia

---
+++ client/src/components/DetailCard/DetailCard.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import DetailCard from "./DetailCard";
+
+test("renders detail card component", () => {
+	render(<DetailCard />);
+});
