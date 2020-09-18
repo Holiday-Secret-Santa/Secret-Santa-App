7df98015a09a126e01410cc778fe5ac1042933d1
KEDuran <krystal.e.garcia@gmail.com>
2020-09-17 20:08:35 -0500

    created basic events page

--- client/src/pages/Events/Events.js
+++ client/src/pages/Events/Events.js
@@ -1,8 +1,24 @@
 import React from "react";
+import { Row, Col } from "antd";
+import CardComp from "../../components/CardComp";
 import "./style.css";
 
 const EventsPage = () => {
-	return <h1>Events Page</h1>;
+	return (
+		<div>
+			<Row>
+				<h1>Events Page</h1>
+			</Row>
+			<Row>
+				<Col span={12}>
+					<CardComp />
+				</Col>
+				<Col span={12}>
+					<CardComp />
+				</Col>
+			</Row>
+		</div>
+	);
 };
 
 export default EventsPage;
