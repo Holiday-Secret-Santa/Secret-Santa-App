9365a7baf90b1d215fdb4716e46b807d436ed29c
Yadira Tamarit <ystamaritq@gmail.com>
2020-09-18 11:45:23 -0500

    style table component with pagination

--- client/src/components/Table/TableComp.js
+++ client/src/components/Table/TableComp.js
@@ -2,14 +2,7 @@ import React from "react";
 import { Table } from "antd";
 
 const TableComp = ({ dataSource, columns }) => {
-	return (
-		<Table
-			dataSource={dataSource}
-			columns={columns}
-			pagination={{ pageSize: 50 }}
-			scroll={{ y: 240 }}
-		/>
-	);
+	return <Table dataSource={dataSource} columns={columns} />;
 };
 
 export default TableComp;
