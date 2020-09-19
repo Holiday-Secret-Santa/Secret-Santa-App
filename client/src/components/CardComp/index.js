c654b9f43aece28c76a5ce23be039ce7930c4fec
KEDuran <krystal.e.garcia@gmail.com>
2020-09-18 19:02:47 -0500

    Revert "modified for code climate"
    This reverts commit 636dc16ea622844ce4eea4cc8bc356b97f195fb2.

--- client/src/components/CardComp/index.js
+++ client/src/components/CardComp/index.js
@@ -1,24 +1,7 @@
 import React from "react";
-import { Card, Col, Space } from "antd";
-import "./style.css";
+import { Card } from "antd";
 
-// Responsiveness column
-const ResponsiveColumn = ({ children, lg, offset, span, flex, style }) => {
-	return (
-		<Col
-			xs={24}
-			sm={24}
-			md={8}
-			lg={8}
-			offset={offset}
-			span={span}
-			flex={flex}
-			style={style}
-		>
-			{children}
-		</Col>
-	);
-};
+import "./style.css";
 
 // placeholder parameters for API data
 const exchangeCard = ({
@@ -31,26 +14,21 @@ const exchangeCard = ({
 	actions,
 }) => (
 	// Basic Ant card components (will update design during development)
-	<ResponsiveColumn>
-		<Space>
-			<Card
-			title={description}
-			actions={actions}
-			style={{ width: "100%", fontSize: "18px" }}
-			headStyle={{
-				backgroundColor: "#d62828",
-				color: "#ffffff",
-				fontSize: "20px",
-			}}
-		>
-			<p>Event Date: {date}</p>
-			<p>Event Time:{startTime}</p>
-			<p>Location: {location}</p>
-			<p>Total Participants: {participants}</p>
-		</Card>
-		</Space>
-		
-	</ResponsiveColumn>
+	<Card
+		title={description}
+		actions={actions}
+		style={{ width: "100%", fontSize: "18px" }}
+		headStyle={{
+			backgroundColor: "#d62828",
+			color: "#ffffff",
+			fontSize: "20px",
+		}}
+	>
+		<p>Event Date: {date}</p>
+		<p>Event Time:{startTime}</p>
+		<p>Location: {location}</p>
+		<p>Total Participants: {participants}</p>
+	</Card>
 );
 
 // Exporting component
