7383cd3ac86b365ea36ae5f9b09623e1415bf2b3
KEDuran <krystal.e.garcia@gmail.com>
2020-09-19 14:49:15 -0500

    added basic form for add gift page

--- client/src/pages/AddGift/AddGift.js
+++ client/src/pages/AddGift/AddGift.js
@@ -1,8 +1,99 @@
 import React from "react";
+import { Row, Col, Form, Input, InputNumber, Button } from "antd";
+import { PlusCircleOutlined } from "@ant-design/icons";
 import "./style.css";
 
+// Variable to for responsive columns
+const ResponsiveColumn = ({ children, lg, offset, span, flex, style }) => {
+	return (
+		<Col
+			xs={24}
+			sm={24}
+			md={8}
+			lg={8}
+			offset={offset}
+			span={span}
+			flex={flex}
+			style={style}
+		>
+			{children}
+		</Col>
+	);
+};
+
+// Creates the Add Gift Pages
 const AddGift = () => {
-	return <h1>Add Gift Ideas</h1>;
+	return (
+		<div>
+			<Row style={{ padding: 15 }}>
+				<ResponsiveColumn>
+					<h1>Add Gift Ideas</h1>
+				</ResponsiveColumn>
+			</Row>
+			<Row style={{ padding: 15 }}>
+				<ResponsiveColumn>
+					{/* Gift Input Form */}
+					<Form name="nest-messages">
+						{/* Input for gift description*/}
+						<Form.Item
+							name="description"
+							label="Gift Description"
+							rules={[
+								{
+									required: true,
+									message: "Please enter gift description.",
+								},
+							]}
+						>
+							<Input />
+						</Form.Item>
+						{/*Input for URL link to gift  */}
+						<Form.Item
+							name="urlLink"
+							label="Link to Gift"
+							rules={[
+								{
+									required: true,
+									message: "Please enter link to gift.",
+								},
+							]}
+						>
+							<Input />
+						</Form.Item>
+						{/* Input for gift price */}
+						<Form.Item
+							name="price"
+							label="Gift Price"
+							rules={[
+								{
+									required: true,
+									type: "number",
+									min: 1,
+									max: 100,
+									message: "Please enter price between $1 and $100.",
+								},
+							]}
+						>
+							<InputNumber />
+						</Form.Item>
+						{/* Submit Button */}
+						<Button
+							type="primary"
+							shape="round"
+							icon={<PlusCircleOutlined />}
+							size="large"
+							style={{
+								backgroundColor: "#2c6e49",
+								borderColor: "#2c6e49",
+							}}
+						>
+							Add Gift
+						</Button>
+					</Form>
+				</ResponsiveColumn>
+			</Row>
+		</div>
+	);
 };
 
 export default AddGift;
