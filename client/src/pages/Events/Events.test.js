4e61a24a8678b5e711ddd0321b416a58fd95baed
Yadira Tamarit <ystamaritq@gmail.com>
2020-09-18 21:20:25 -0500

    Remove duplicate mock of matchMedia

--- client/src/pages/Events/Events.test.js
+++ client/src/pages/Events/Events.test.js
@@ -4,18 +4,5 @@ import Events from "./Events";
 
 // mvp test scenario for events page
 test("renders events dashboard", () => {
-	Object.defineProperty(window, "matchMedia", {
-		writable: true,
-		value: jest.fn().mockImplementation((query) => ({
-			matches: false,
-			media: query,
-			onchange: null,
-			addListener: jest.fn(), // deprecated
-			removeListener: jest.fn(), // deprecated
-			addEventListener: jest.fn(),
-			removeEventListener: jest.fn(),
-			dispatchEvent: jest.fn(),
-		})),
-	});
 	render(<Events />);
 });
