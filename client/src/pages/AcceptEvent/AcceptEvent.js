154c0657b278135dd4f819e3c528a833877db3ad
Yadira Tamarit <ystamaritq@gmail.com>
2020-09-20 15:56:51 -0500

    Develop the accept event page

--- client/src/pages/AcceptEvent/AcceptEvent.js
+++ client/src/pages/AcceptEvent/AcceptEvent.js
@@ -1,8 +1,29 @@
 import React from "react";
+import { Row } from "antd";
+import ResponsiveColumn from "./../../components/ResponsiveColumn";
+import DetailCard from "./../../components/DetailCard/DetailCard";
+import Button from "./../../components/Button/Button";
+import "./style.css";
+
 import "./style.css";
 
 const AcceptEvent = () => {
-	return <h1>Accept Event Page</h1>;
+	return (
+		<>
+			<Row gutter={[30, 30]} justify="center">
+				<ResponsiveColumn style={{ padding: 40 }} lg={8} md={8}>
+					<DetailCard
+						title="You are invited"
+						date="date"
+						startTime="9:00 am"
+						location="My House"
+						participants="12 people"
+						actions={[<Button text="Accept" />, <Button text="Not Going" />]}
+					/>
+				</ResponsiveColumn>
+			</Row>
+		</>
+	);
 };
 
 export default AcceptEvent;
