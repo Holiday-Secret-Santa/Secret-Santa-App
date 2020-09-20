609072ef7e4bcbc3c0cd0336bfacb1d43fb1b418
Yadira Tamarit <ystamaritq@gmail.com>
2020-09-19 22:09:38 -0500

    Refactor AddGift page to have shorter functions

--- client/src/pages/AddGift/AddGift.js
+++ client/src/pages/AddGift/AddGift.js
@@ -6,6 +6,61 @@ import FormInputNumber from "./../../components/FormInputNumber";
 import ResponsiveColumn from "./../../components/ResponsiveColumn";
 import "./style.css";
 
+const SubmitButton = () => {
+	return (
+		<Button
+			type="primary"
+			shape="round"
+			icon={<PlusCircleOutlined />}
+			size="large"
+			style={{
+				backgroundColor: "#2c6e49",
+				borderColor: "#2c6e49",
+			}}
+		>
+			Add Gift
+		</Button>
+	);
+};
+
+const DescriptionField = () => {
+	return (
+		<FormInputText
+			name="description"
+			label="Description"
+			rules={[{ required: true, message: "please enter gift description" }]}
+		/>
+	);
+};
+
+const GiftLink = () => {
+	return (
+		<FormInputText
+			name="url"
+			label="Link"
+			rules={[{ required: true, message: "please enter link to gift" }]}
+		/>
+	);
+};
+
+const GiftPrice = () => {
+	return (
+		<FormInputNumber
+			name="price"
+			label="Price"
+			rules={[
+				{
+					required: true,
+					type: "number",
+					min: 1,
+					max: 100,
+					message: "please enter price between $1 and $100",
+				},
+			]}
+		/>
+	);
+};
+
 // Creates the Add Gift Pages
 const AddGift = () => {
 	return (
@@ -16,46 +71,13 @@ const AddGift = () => {
 					<Form name="nest-messages">
 						<h1>Add Gift Ideas</h1>
 						{/* Input for gift description*/}
-						<FormInputText
-							name="description"
-							label="Description"
-							rules={[
-								{ required: true, message: "please enter gift description" },
-							]}
-						/>
+						<DescriptionField />
 						{/* Input link to gift*/}
-						<FormInputText
-							name="url"
-							label="Link"
-							rules={[{ required: true, message: "please enter link to gift" }]}
-						/>
+						<GiftLink />
 						{/* Input gift price */}
-						<FormInputNumber
-							name="price"
-							label="Price"
-							rules={[
-								{
-									required: true,
-									type: "number",
-									min: 1,
-									max: 100,
-									message: "please enter price between $1 and $100",
-								},
-							]}
-						/>
+						<GiftPrice />
 						{/* Submit Button */}
-						<Button
-							type="primary"
-							shape="round"
-							icon={<PlusCircleOutlined />}
-							size="large"
-							style={{
-								backgroundColor: "#2c6e49",
-								borderColor: "#2c6e49",
-							}}
-						>
-							Add Gift
-						</Button>
+						<SubmitButton />
 					</Form>
 				</ResponsiveColumn>
 			</Row>
