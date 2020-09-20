8865c2bff637248d03b38508591ea71398212b79
KEDuran <krystal.e.garcia@gmail.com>
2020-09-20 18:51:25 -0500

    updated layout of profile page

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Image } from "antd";
+import { Image, Descriptions } from "antd";
+import ResponsiveColumn from "../../components/ResponsiveColumn";
 
 const ProfilePage = () => {
 	const { user } = useAuth0();
@@ -8,10 +9,20 @@ const ProfilePage = () => {
 
 	return (
 		<div>
-			<h3>Profile</h3>
-			<Image src={picture} />
-			<p>{name}</p>
-			<p>{email}</p>
+			<Row style={{ padding: 20 }}>
+				<ResponsiveColumn>
+					<h3>Profile</h3>
+				</ResponsiveColumn>
+			</Row>
+			<Row gutter={[30, 30]} style={{ padding: 20 }}>
+				<ResponsiveColumn>
+					<Image src={picture} />
+					<Descriptions title="User Info">
+						<Descriptions.Item label="UserName">{name}</Descriptions.Item>
+						<Descriptions.Item label="Email">{email}</Descriptions.Item>
+					</Descriptions>
+				</ResponsiveColumn>
+			</Row>
 		</div>
 	);
 };
