b7119e403b0dd09233cd2841cbbf502d3496a739
Yadira Tamarit <ystamaritq@gmail.com>
2020-09-20 17:16:53 -0500

    Add rsvp page

---
+++ client/src/pages/RsvpEvent/RsvpEvent.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Row, notification } from "antd";
+import ResponsiveColumn from "../../components/ResponsiveColumn";
+import DetailCard from "../../components/DetailCard/DetailCard";
+import Button from "../../components/Button";
+import "./style.css";
+
+const processStatusAction = (status, setStatusCallBack) => {
+	if (status === "going") {
+		notification.success({
+			message: "Going",
+			description: "You Have been added to the event list",
+		});
+	} else if (status === "not going") {
+		notification.warn({
+			message: "Not Going",
+			description: "No worries, we will notify the event host",
+		});
+	}
+	setStatusCallBack(status);
+};
+
+const RsvpEvent = () => {
+	const [status, setStatus] = useState(null);
+	return (
+		<>
+			<Row gutter={[30, 30]} justify="center">
+				<ResponsiveColumn style={{ padding: 40 }} lg={9} md={9}>
+					<DetailCard
+						title="You are invited"
+						date="date"
+						startTime="9:00 am"
+						location="My House"
+						participants="12 people"
+						actions={[
+							<Button
+								text="Going"
+								disabled={status === "going"}
+								action={() => processStatusAction("going", setStatus)}
+							/>,
+							<Button
+								text="Not Going"
+								disabled={status === "not going"}
+								bgColor="#D62828"
+								action={() => processStatusAction("not going", setStatus)}
+							/>,
+						]}
+					/>
+				</ResponsiveColumn>
+			</Row>
+		</>
+	);
+};
+
+export default RsvpEvent;
--- client/src/pages/RsvpEvent/RsvpEvent.test.js
+++ client/src/pages/RsvpEvent/RsvpEvent.test.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import AcceptEvent from "./AcceptEvent";
+import RsvpEvent from "./RsvpEvent";
 
 // mvp test scenario for accept events page
-test("renders accept event page", () => {
-	render(<AcceptEvent />);
+test("renders rsvp event page", () => {
+	render(<RsvpEvent />);
 });
