798cce5971345d106cf733a2a947fba5577cf141
Valeria Gonzalez Leyva <valeglez.leyva@gmail.com>
2020-09-21 09:35:52 -0500

    Created component

---
+++ client/src/components/WelcomeMessage/WelcomeMsg.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+function WelcomeMsg() {
+    return (
+        <h1 className="message">Making the Secret Santa experience<br/>a little easier for you!</h1>
+    )
+}
+
+export default WelcomeMsg;
--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,7 +1,29 @@
 import React from "react";
+import {Col, Row, Layout} from "antd";
+import CarouselPage from "../../components/Carousel/CarouselPage";
+import WelcomeMsg from "../../components/WelcomeMessage/WelcomeMsg";
+import HomePageImgs from "../../components/HomePageImgs/HomePageImgs";
 
 const HomePage = () => {
-	return <h1>Home Page</h1>;
+	return (
+    <Layout fluid className="p-0">
+      <Row>
+        <Col className="col-12">
+          <CarouselPage />
+        </Col>
+      </Row>
+      <Row justify="center">
+        <Col>
+          <WelcomeMsg className="col-12" />
+        </Col>
+      </Row>
+      <Row justify="center">
+        <Col>
+          <HomePageImgs />
+        </Col>
+      </Row>
+    </Layout>
+  );
 };
 
 export default HomePage;
