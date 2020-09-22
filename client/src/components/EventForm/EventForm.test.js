58654bf588b83ac6c64a9137a84be373e1eff773
JDMartinez1531 <63159799+JDMartinez1531@users.noreply.github.com>
2020-09-22 12:00:35 -0500

    developed user text input test, refactored event form test with describe block

--- client/src/components/EventForm/EventForm.test.js
+++ client/src/components/EventForm/EventForm.test.js
@@ -1,9 +1,21 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event";
 import EventForm from "./index.js";
 
-test("renders form with header", () => {
-  render(<EventForm />);
-  expect(screen.getByText("Create New Event")).toBeInTheDocument();
+describe("New Event Form", () => {
+
+  test("renders form with header", () => {
+    render(<EventForm />);
+    expect(screen.getByText("Create New Event")).toBeInTheDocument();
+  });
+  
+  test("user text inputs", () => {
+    render(<EventForm />);
+    userEvent.type(screen.getByLabelText("Your Event Title"), "Hello, World!");
+    expect(
+      screen.getByRole("textbox", { name: /Your Event Title/i })
+    ).toHaveValue("Hello, World!");
+  });
 });
