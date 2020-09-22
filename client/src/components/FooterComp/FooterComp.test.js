1065b57eeff8aa4aed8dee78c7eec65a19260578
KEDuran <krystal.e.garcia@gmail.com>
2020-09-22 13:18:45 -0500

    added new test file for FooterComp

---
+++ client/src/components/FooterComp/FooterComp.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import FooterComp from "./FooterComp";
+
+test("renders detail card component", () => {
+	render(<FooterComp />);
+});
