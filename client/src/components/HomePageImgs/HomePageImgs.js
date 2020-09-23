8b29da197f8119d45ac5598ce707f56f7002a5a1
Valeria Gonzalez Leyva <valeglez.leyva@gmail.com>
2020-09-22 19:01:52 -0500

    Added CardHome component

--- client/src/components/HomePageImgs/HomePageImgs.js
+++ client/src/components/HomePageImgs/HomePageImgs.js
@@ -7,24 +7,25 @@ import gifts3 from "./imgs/gifts3.jpg";
 import "./style.css";
 const { Meta } = Card;
 
+const CardHome = (props) => {
+  return (
+    <Card hoverable className="image-card" cover={<img alt={props.alt} src={props.src} />}>
+      <Meta title={props.title} description={props.description} />
+    </Card>);
+};
+
 function HomePageImgs() {
   return (
     <div>
       <Row gutter={[30, 30]} style={{ padding: 20 }}>
         <ResponsiveColumn lg={8}>
-          <Card hoverable className="image-card" cover={<img alt="gifts3" src={gifts3} />}>
-            <Meta title="Create or join an event" description="You can create, invite and manage your event's guests, or join the party!" />
-          </Card>
+          {<CardHome alt="gifts3" src={gifts3} title="Create or join an event" description="You can create, invite and manage your event's guests, or join the party!" />}
         </ResponsiveColumn>
         <ResponsiveColumn lg={8}>
-          <Card hoverable className="image-card" cover={<img alt="gifts2" src={gifts2} />}>
-            <Meta title="Gift Selection" description="Access your Secret Santa's wish list and purchase via the app!" />
-          </Card>
+          {<CardHome alt="gifts2" src={gifts2} title="Gift Selection" description="Access your Secret Santa's wish list and purchase via the app!" />}
         </ResponsiveColumn>
         <ResponsiveColumn lg={8}>
-          <Card hoverable className="image-card" cover={<img alt="friends" src={friends} />} bordered={false}>
-            <Meta title="Secret Santa Participants" description="See who else is participating and who your Secret Santa might be!" />
-          </Card>
+          {<CardHome alt="friends" src={friends} title="Secret Santa Participants" description="See who else is participating and who your Secret Santa might be!" />}
         </ResponsiveColumn>
       </Row>
     </div>
