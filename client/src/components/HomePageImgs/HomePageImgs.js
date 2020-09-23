efd551d01ad784ed72a4c21cdfc3f8d2f5ca6e92
Valeria Gonzalez Leyva <valeglez.leyva@gmail.com>
2020-09-22 20:51:55 -0500

    refactored duplicate code

--- client/src/components/HomePageImgs/HomePageImgs.js
+++ client/src/components/HomePageImgs/HomePageImgs.js
@@ -1,35 +1,42 @@
-import React from "react";
+import React, {Component} from "react";
 import { Row, Card } from "antd";
 import ResponsiveColumn from "../ResponsiveColumn/index";
-import friends from "./imgs/friends.jpg";
-import gifts2 from "./imgs/gifts2.jpg";
-import gifts3 from "./imgs/gifts3.jpg";
+import images from "./imageData.json";
 import "./style.css";
+
 const { Meta } = Card;
 
 const CardHome = (props) => {
   return (
-    <Card hoverable className="image-card" cover={<img alt={props.alt} src={props.src} />}>
+    <Card className="image-card" hoverable key={props.id} cover={<img alt={props.alt} src={props.image} />}>
       <Meta title={props.title} description={props.description} />
     </Card>);
 };
 
-function HomePageImgs() {
-  return (
-    <div>
-      <Row gutter={[30, 30]} style={{ padding: 20 }}>
-        <ResponsiveColumn lg={8}>
-          {<CardHome alt="gifts3" src={gifts3} title="Create or join an event" description="You can create, invite and manage your event's guests, or join the party!" />}
-        </ResponsiveColumn>
-        <ResponsiveColumn lg={8}>
-          {<CardHome alt="gifts2" src={gifts2} title="Gift Selection" description="Access your Secret Santa's wish list and purchase via the app!" />}
-        </ResponsiveColumn>
-        <ResponsiveColumn lg={8}>
-          {<CardHome alt="friends" src={friends} title="Secret Santa Participants" description="See who else is participating and who your Secret Santa might be!" />}
-        </ResponsiveColumn>
-      </Row>
-    </div>
-  );
+class HomePageImgs extends Component {
+    state = {
+      images
+    };
+
+    render () {
+      return (
+        <div>
+          <Row gutter={[30, 30]} style={{ padding: 20 }}>
+            {this.state.images.map(image => (
+              <ResponsiveColumn lg={8}>
+                <CardHome 
+                  id={image.id}
+                  alt={image.alt}
+                  title={image.title}
+                  description={image.description}
+                  image={image.photo}
+                />
+              </ResponsiveColumn>
+            ))}
+          </Row>
+        </div>
+      );
+    }
 }
 
 export default HomePageImgs;
