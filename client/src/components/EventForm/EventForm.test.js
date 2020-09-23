96ff3610b0bf7d9d1e783f1ecd219f9bb1818e93
JDMartinez1531 <63159799+JDMartinez1531@users.noreply.github.com>
2020-09-23 15:02:26 -0500

    addguestfield testing

--- client/src/components/EventForm/EventForm.test.js
+++ client/src/components/EventForm/EventForm.test.js
@@ -3,7 +3,9 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import userEvent from "@testing-library/user-event";
 import EventForm from "./index.js";
+import { Form } from "antd";
 import ImportGuestList from "../ImportGuestList";
+import AddguestField from "../AddGuestField/index";
 
 describe("New Event Form", () => {
   test("renders form with header", () => {
@@ -31,5 +33,9 @@ describe("New Event Form", () => {
 
   test("renders add guest card", () => {
     render(<ImportGuestList />);
+  });
+
+  test("renders AddguestField component", () => {
+    render(<Form><AddguestField /></Form>);
   });
 });
