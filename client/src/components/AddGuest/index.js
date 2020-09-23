972fb1fc57a9245d17bdd5d5da14d46f40cbcfee
JDMartinez1531 <63159799+JDMartinez1531@users.noreply.github.com>
2020-09-22 21:18:25 -0500

    removed add guest email from import card

--- client/src/components/AddGuest/index.js
+++ client/src/components/AddGuest/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, Input, Button, Upload, Card } from "antd";
+import { Button, Upload, Card } from "antd";
 import { UploadOutlined } from "@ant-design/icons";
 import "antd/dist/antd.css";
 import "./style.css";
@@ -10,11 +10,6 @@ const AddGuests = () => {
       <Upload>
         <Button icon={<UploadOutlined />}>Click to Upload</Button>
       </Upload>
-      <p className="add-guest-text">Or add guest email</p>
-      <Form.Item>
-        <Input type="text" placeholder="Guest Email"></Input>
-        <Button type="primary">Add Guest</Button>
-      </Form.Item>
     </Card>
   );
 };
