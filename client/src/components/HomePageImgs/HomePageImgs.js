13e571a1ee2be489f0b7ba228f66a476468d5ff2
Valeria Gonzalez Leyva <valeglez.leyva@gmail.com>
2020-09-22 21:20:02 -0500

    refactored duplicate code

--- client/src/components/HomePageImgs/HomePageImgs.js
+++ client/src/components/HomePageImgs/HomePageImgs.js
@@ -1,34 +1,42 @@
-import React from "react";
+import React, {Component} from "react";
 import { Row, Card } from "antd";
 import ResponsiveColumn from "../ResponsiveColumn/index";
-import friends from "./imgs/friends.jpg";
-import gifts2 from "./imgs/gifts2.jpg";
-import gifts3 from "./imgs/gifts3.jpg";
+import images from "./imageData.json";
 import "./style.css";
+
 const { Meta } = Card;
 
-function HomePageImgs() {
+const CardHome = (props) => {
   return (
-    <div>
-      <Row gutter={[30, 30]} style={{ padding: 20 }}>
-        <ResponsiveColumn lg={8}>
-          <Card hoverable className="image-card" cover={<img alt="gifts3" src={gifts3} />}>
-            <Meta title="Create or join an event" description="You can create, invite and manage your event's guests, or join the party!" />
-          </Card>
-        </ResponsiveColumn>
-        <ResponsiveColumn lg={8}>
-          <Card hoverable className="image-card" cover={<img alt="gifts2" src={gifts2} />}>
-            <Meta title="Gift Selection" description="Access your Secret Santa's wish list and purchase via the app!" />
-          </Card>
-        </ResponsiveColumn>
-        <ResponsiveColumn lg={8}>
-          <Card hoverable className="image-card" cover={<img alt="friends" src={friends} />} bordered={false}>
-            <Meta title="Secret Santa Participants" description="See who else is participating and who your Secret Santa might be!" />
-          </Card>
-        </ResponsiveColumn>
-      </Row>
-    </div>
-  );
+    <Card className="image-card" hoverable key={props.id} cover={<img alt={props.alt} src={props.image} />}>
+      <Meta title={props.title} description={props.description} />
+    </Card>);
+};
+
+class HomePageImgs extends Component {
+    state = {
+      images
+    };
+
+    render () {
+      return (
+        <div>
+          <Row gutter={[30, 30]} style={{ padding: 20 }}>
+            {this.state.images.map(image => (
+              <ResponsiveColumn lg={8}>
+                <CardHome 
+                  id={image.id}
+                  alt={image.alt}
+                  title={image.title}
+                  description={image.description}
+                  image={image.photo}
+                />
+              </ResponsiveColumn>
+            ))}
+          </Row>
+        </div>
+      );
+    }
 }
 
 export default HomePageImgs;
