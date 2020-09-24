70acf346a0909390404718034509a2572b7c4972
KEDuran <krystal.e.garcia@gmail.com>
2020-09-23 20:16:45 -0500

    added a render test for the modal

---
+++ client/src/components/ModalPopUp/ModalPopUp.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import ModalPopUp from "./ModalPopUp";
+
+test("renders detail card component", () => {
+	render(<ModalPopUp />);
+});
