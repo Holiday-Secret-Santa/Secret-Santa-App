d76b956946f6933f388a57c3b1a6f6f3f2c3760c
JDMartinez1531 <63159799+JDMartinez1531@users.noreply.github.com>
2020-09-24 21:29:44 -0500

    added htmlType prop to button component in order to utilize antd default form behavior

--- client/src/pages/AddGift/AddGift.js
+++ client/src/pages/AddGift/AddGift.js
@@ -6,43 +6,46 @@ import { AddButton } from "./../../components/Button";
 import "./style.css";
 
 const descriptionFieldRules = [
-	{ required: true, message: "please enter gift description" },
+  { required: true, message: "please enter gift description" },
 ];
 
 const giftLinkRules = [
-	{ required: true, message: "please enter link to gift" },
+  { required: true, message: "please enter link to gift" },
 ];
 
 const giftPriceRules = [
-	{
-		required: true,
-		type: "number",
-		min: 1,
-		max: 100,
-		message: "please enter price between $1 and $100",
-	},
+  {
+    required: true,
+    type: "number",
+    min: 1,
+    max: 100,
+    message: "please enter price between $1 and $100",
+  },
 ];
 
 // Creates the Add Gift Pages
 const AddGift = () => {
-	return (
-		<Row gutter={[30, 30]} style={{ padding: 20 }} className="addGiftForm">
-			<ResponsiveColumn lg={24}>
-				<Form name="nest-messages">
-					<h1>Add Gift Ideas</h1>
-					{/* Input for gift description*/}
-					<FormInputText
-						name="description"
-						label="Description"
-						rules={descriptionFieldRules}
-					/>
-					<FormInputText name="url" label="Link" rules={giftLinkRules} />
-					<FormInputNumber name="price" label="Price" rules={giftPriceRules} />
-					<AddButton text="Add Gift" />
-				</Form>
-			</ResponsiveColumn>
-		</Row>
-	);
+  const onFinish = values => {
+    console.log("Success:", values);
+  };
+  return (
+    <Row gutter={[30, 30]} style={{ padding: 20 }} className="addGiftForm">
+      <ResponsiveColumn lg={24}>
+        <Form onFinish={onFinish} name="nest-messages">
+          <h1>Add Gift Ideas</h1>
+          {/* Input for gift description*/}
+          <FormInputText
+            name="description"
+            label="Description"
+            rules={descriptionFieldRules}
+          />
+          <FormInputText name="url" label="Link" rules={giftLinkRules} />
+          <FormInputNumber name="price" label="Price" rules={giftPriceRules} />
+          <AddButton htmlType="submit" text="Add Gift" />
+        </Form>
+      </ResponsiveColumn>
+    </Row>
+  );
 };
 
 export default AddGift;
--- graphqlSchema/schema.js
+++ graphqlSchema/schema.js
@@ -1,7 +1,7 @@
 const { buildSchema } = require("graphql");
+const { random } = require("lodash");
 const _ = require("lodash");
 const db = require("./../models");
-
 var schema = buildSchema(`
 	input InputEvent {
 		date: String,
@@ -11,7 +11,6 @@ var schema = buildSchema(`
 		location: String,
 		planner_email: String,
 	}
-
 	type Event {
 		id: Int,
 		date: String,
@@ -21,7 +20,6 @@ var schema = buildSchema(`
 		location: String,
 		planner_email: String,
 	}
-
 	input InputParticipant {
 		first_name: String,
 		last_name: String,
@@ -32,7 +30,6 @@ var schema = buildSchema(`
 		EventId: Int,
 		secret_santa_id: Int
 	}
-
 	type Participant {
 		id: Int,
 		first_name: String,
@@ -44,22 +41,23 @@ var schema = buildSchema(`
 		EventId: Int,
 		secret_santa_id: Int
 	}
-
+	type ParticipantSanata {
+		participant_id: Int,
+		secret_santa_id: Int
+	}
 	input InputGift {
 		description: String,
 		link: String,
 		price: Int,
-		Participant_id: Int
+		ParticipantId: Int
 	}
-
 	type Gift {
 		id: Int,
 		description: String,
 		link: String,
 		price: Int,
-		Participant_id: Int
+		ParticipantId: Int
 	}
-	
 	type Query {
 		getEvents: [Event],
 		getEvent(id: Int): Event, 
@@ -69,9 +67,7 @@ var schema = buildSchema(`
 		getGifts: [Gift],
 		getGift(id: Int): Gift,
 		getGiftByParticipantId(participant_id: Int): [Gift],
-		
 	}
-
 	type Mutation {
 		createEvent(input: InputEvent): Event,
 		deleteEvent(id: Int): Int,
@@ -79,69 +75,87 @@ var schema = buildSchema(`
 		deleteParticipant(id: Int): Int, 
 		createGift(input: InputGift): Gift,
 		deleteGift(id: Int): Int,
-		assignSecretSanta(participant_id: Int, secret_santa_id: Int): [Int] 
+		assignSecretSanta(participant_id: Int, secret_santa_id: Int): [Int],
+		autoAssignSecretSanta(eventId: Int): [ParticipantSanata],
 	} 
-
 `);
-
-var root = {
-	getEvents: () => {
-		return db.Event.findAll();
-	},
-	getEvent: ({ id }) => {
-		return db.Event.findOne({ where: { id: id } });
-	},
-	createEvent: ({ input }) => {
-		return db.Event.create(input);
-	},
-	deleteEvent: ({ id }) => {
-		return db.Event.destroy({ where: { id: id } });
-	},
-	getParticipants: () => {
-		return db.Participant.findAll();
-	},
-	getParticipantsByEventId: ({ eventId }) => {
-		return db.Participant.findAll({ where: { EventId: eventId } });
-	},
-	getParticipant: ({ id }) => {
-		return db.Participant.findOne({ where: { id: id } });
-	},
-	createParticipant: ({ input }) => {
-		return db.Participant.create(input);
-	},
-	deleteParticipant: ({ id }) => {
-		return db.Participant.destroy({ where: { id: id } });
-	},
-	getGifts: () => {
-		return db.Gift.findAll();
-	},
-	getGift: ({ id }) => {
-		return db.Gift.findOne({ where: { id: id } });
-	},
-	createGift: ({ input }) => {
-		return db.Gift.create(input);
-	},
-	deleteGift: ({ id }) => {
-		return db.Gift.destroy({ where: { id: id } });
-	},
-	getGiftByParticipantId: ({ participant_id }) => {
-		return db.Gift.findAll({
-			where: { Participant_id: participant_id },
-		});
-	},
-	assignSecretSanta: ({ participant_id, secret_santa_id }) => {
-		return db.Participant.update(
-			{ secret_santa_id: secret_santa_id },
-			{
-				where: {
-					id: participant_id,
-				},
-			}
-		);
-	},
+const getParticipantsByEventId = (eventId) => {
+  return db.Participant.findAll({ where: { EventId: eventId } });
 };
-
+var root = {
+  getEvents: () => {
+    return db.Event.findAll();
+  },
+  getEvent: ({ id }) => {
+    return db.Event.findOne({ where: { id: id } });
+  },
+  createEvent: ({ input }) => {
+    return db.Event.create(input);
+  },
+  deleteEvent: ({ id }) => {
+    return db.Event.destroy({ where: { id: id } });
+  },
+  getParticipants: () => {
+    return db.Participant.findAll();
+  },
+  getParticipantsByEventId: ({ eventId }) => {
+    return getParticipantsByEventId(eventId);
+  },
+  getParticipant: ({ id }) => {
+    return db.Participant.findOne({ where: { id: id } });
+  },
+  createParticipant: ({ input }) => {
+    return db.Participant.create(input);
+  },
+  deleteParticipant: ({ id }) => {
+    return db.Participant.destroy({ where: { id: id } });
+  },
+  getGifts: () => {
+    return db.Gift.findAll();
+  },
+  getGift: ({ id }) => {
+    return db.Gift.findOne({ where: { id: id } });
+  },
+  createGift: ({ input }) => {
+    return db.Gift.create(input);
+  },
+  deleteGift: ({ id }) => {
+    return db.Gift.destroy({ where: { id: id } });
+  },
+  getGiftByParticipantId: ({ participant_id }) => {
+    return db.Gift.findAll({
+      where: { ParticipantId: participant_id },
+    });
+  },
+  assignSecretSanta: ({ participant_id, secret_santa_id }) => {
+    return db.Participant.update(
+      { secret_santa_id: secret_santa_id },
+      {
+        where: {
+          id: participant_id,
+        },
+      }
+    );
+  },
+  autoAssignSecretSanta: async ({ eventId }) => {
+    let results = await getParticipantsByEventId(eventId);
+    if (results.length < 2) {return [];}
+    const participantIds = results.map((r) => r.dataValues.id);
+    var availableSantas = [...participantIds];
+    return participantIds.map((p, i) => {
+      var santaIndex = 0;
+      do {
+        santaIndex = random(Math.max(availableSantas.length - 1, 0));
+      } while (availableSantas[santaIndex] === p && availableSantas.length > 1);
+      let santa_id = availableSantas.splice(santaIndex, 1)[0];
+      return {
+        participant_id: p,
+        secret_santa_id: santa_id,
+      };
+    });
+  },
+};
 module.exports = {
-	schema,
-	root,
+  schema,
+  root,
 };
