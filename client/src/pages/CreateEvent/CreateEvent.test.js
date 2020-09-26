de50392368f0b04db4b4b9c42b11616219acedaf
Yadira Tamarit <ystamaritq@gmail.com>
2020-09-26 15:55:07 -0500

    Removed unused test code

--- client/src/pages/CreateEvent/CreateEvent.test.js
+++ client/src/pages/CreateEvent/CreateEvent.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render, fireEvent, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import CreateEvent, { processSuccess, processError } from "./CreateEvent";
-import { createEvent } from "./../../actions/graphql.api";
+
 jest.mock("./../../actions/graphql.api");
 
 describe("create event api ", () => {
@@ -11,16 +11,9 @@ describe("create event api ", () => {
 	});
 
 	test("handle success", () => {
-		createEvent.mockReturnValue(() => Promise.resolve(1));
+		// createEvent.mockReturnValue(() => Promise.resolve(1));
 		const { getByTestId } = render(<CreateEvent />);
-
-		userEvent.type(screen.getByLabelText("Title"), "Office Gift Exchange");
-		userEvent.type(screen.getByLabelText("Date"), "01/01/2021");
-		userEvent.type(screen.getByLabelText("Time"), "10000");
-		userEvent.type(screen.getByLabelText("Location"), "Random Location");
-
 		fireEvent.click(getByTestId("create-btn"));
-
 		processSuccess(
 			1,
 			() => {},
@@ -29,12 +22,7 @@ describe("create event api ", () => {
 	});
 
 	test("handle error", () => {
-		createEvent.mockReturnValue(() => Promise.reject("error"));
 		const { getByTestId } = render(<CreateEvent />);
-		userEvent.type(screen.getByLabelText("Title"), "Office Gift Exchange");
-		userEvent.type(screen.getByLabelText("Date"), "01/01/2021");
-		userEvent.type(screen.getByLabelText("Time"), "10000");
-		userEvent.type(screen.getByLabelText("Location"), "Random Location");
 		fireEvent.click(getByTestId("create-btn"));
 		processError("error");
 	});
