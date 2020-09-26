8a2b5f7764de4d9e10eb0375b4a2dd3f11f774b1
Yadira Tamarit <ystamaritq@gmail.com>
2020-09-25 19:51:05 -0500

    Refactor event form into smaller components

--- client/src/components/EventForm/index.js
+++ client/src/components/EventForm/index.js
@@ -3,9 +3,67 @@ import { Form, Row } from "antd";
 import Button from "./../Button";
 import ResponsiveColumn from "./../ResponsiveColumn";
 import { FormInputText, FormInputDate, FormInputTime } from "./../FormInput";
-
 import "antd/dist/antd.css";
 import "./style.css";
+
+const TitleInput = () => {
+	return (
+		<ResponsiveColumn lg={24}>
+			<FormInputText
+				name="title"
+				label="Title"
+				placeholder="Event title"
+				rules={[{ required: true, message: "Please enter a title" }]}
+			/>
+		</ResponsiveColumn>
+	);
+};
+
+const DateInput = () => {
+	return (
+		<ResponsiveColumn lg={24}>
+			<Row justify="space-between" gutter={[10, 10]}>
+				<ResponsiveColumn lg={12}>
+					<FormInputDate
+						name="date"
+						label="Date"
+						rules={[{ required: true, message: "Please enter a date" }]}
+					/>
+				</ResponsiveColumn>
+				<ResponsiveColumn lg={12}>
+					<FormInputTime
+						name="time"
+						label="Time"
+						rules={[{ required: true, message: "Please enter a time" }]}
+					/>
+				</ResponsiveColumn>
+			</Row>
+		</ResponsiveColumn>
+	);
+};
+
+const LocationInput = () => {
+	return (
+		<ResponsiveColumn lg={24}>
+			<FormInputText
+				name="location"
+				label="Location"
+				placeholder="Enter address or virtual meeting link"
+				rules={[{ required: true, message: "Please enter a location" }]}
+			/>
+		</ResponsiveColumn>
+	);
+};
+
+const ButtonInput = ({ action }) => {
+	return (
+		<ResponsiveColumn lg={24}>
+			<Form.Item className="submitBtn">
+				<Button testid="create-btn" text="Create Event" action={action} />
+			</Form.Item>
+		</ResponsiveColumn>
+	);
+};
 
 const EventForm = ({ action }) => {
 	const [form] = Form.useForm();
@@ -19,49 +77,10 @@ const EventForm = ({ action }) => {
 					className="newEventForm"
 				>
 					<Row>
-						<ResponsiveColumn lg={24}>
-							<FormInputText
-								name="title"
-								label="Title"
-								placeholder="Event title"
-								rules={[{ required: true, message: "Please enter a title" }]}
-							/>
-						</ResponsiveColumn>
-						<ResponsiveColumn lg={24}>
-							<Row justify="space-between" gutter={[10, 10]}>
-								<ResponsiveColumn lg={12}>
-									<FormInputDate
-										name="date"
-										label="Date"
-										rules={[{ required: true, message: "Please enter a date" }]}
-									/>
-								</ResponsiveColumn>
-								<ResponsiveColumn lg={12}>
-									<FormInputTime
-										name="time"
-										label="Time"
-										rules={[{ required: true, message: "Please enter a time" }]}
-									/>
-								</ResponsiveColumn>
-							</Row>
-						</ResponsiveColumn>
-						<ResponsiveColumn lg={24}>
-							<FormInputText
-								name="location"
-								label="Location"
-								placeholder="Enter address or virtual meeting link"
-								rules={[{ required: true, message: "Please enter a location" }]}
-							/>
-						</ResponsiveColumn>
-						<ResponsiveColumn lg={24}>
-							<Form.Item className="submitBtn">
-								<Button
-									testid="create-btn"
-									text="Create Event"
-									action={form.submit}
-								/>
-							</Form.Item>
-						</ResponsiveColumn>
+						<TitleInput />
+						<DateInput />
+						<LocationInput />
+						<ButtonInput action={form.submit} />
 					</Row>
 				</Form>
 			</ResponsiveColumn>
