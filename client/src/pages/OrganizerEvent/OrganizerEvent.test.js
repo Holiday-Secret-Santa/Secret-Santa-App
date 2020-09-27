cf7146806f1678cd8612ae5ffb68580266e657af
Yadira Tamarit <ystamaritq@gmail.com>
2020-09-27 08:32:08 -0500

    Increases coverage of Organaizer from 96 to 100%

--- client/src/pages/OrganizerEvent/OrganizerEvent.test.js
+++ client/src/pages/OrganizerEvent/OrganizerEvent.test.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event";
 import OrganizerEvent, {
 	showSuccess,
 	showError,
@@ -14,7 +16,17 @@ jest.mock("./../../actions/graphql.api");
 
 // mvp test scenario for organizer event page
 test("renders organizer event page", () => {
-	render(<OrganizerEvent />);
+	render(<OrganizerEvent match={{ params: { id: 1 } }} />);
+
+	userEvent.click(screen.getByRole("button", { name: /Add Participants/i }));
+	userEvent.type(screen.getByRole("textbox", { name: /First Name/i }), "test");
+	userEvent.type(screen.getByRole("textbox", { name: /Last Name/i }), "test");
+	userEvent.type(
+		screen.getByRole("textbox", { name: /Email/i }),
+		"test@test.com"
+	);
+	userEvent.click(screen.getByRole("button", { name: /OK/i }));
+
 	showSuccess();
 	showError({});
 	getParticipantsInfo();
