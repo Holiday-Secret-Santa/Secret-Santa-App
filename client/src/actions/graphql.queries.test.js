ff4e5686ec5d92373d613be7e8898d81bc885709
KEDuran <krystal.e.garcia@gmail.com>
2020-09-26 19:13:26 -0500

    added new tests

--- client/src/actions/graphql.queries.test.js
+++ client/src/actions/graphql.queries.test.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import { createEventMutation } from "./graphql.queries";
+import {
+	createEventMutation,
+	createParticipantMutation,
+} from "./graphql.queries";
 
 test("validate create event mutation", () => {
 	expect(createEventMutation).toMatch(/mutation CreateEvent/);
+});
+
+test("validate create participant mutation", () => {
+	expect(createParticipantMutation).toMatch(/mutation CreateParticipant/);
 });
