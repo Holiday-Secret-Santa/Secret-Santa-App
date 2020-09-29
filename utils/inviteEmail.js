f83d211f95628c02dd45a4ab5230d6bf853f94d4
KEDuran <krystal.e.garcia@gmail.com>
2020-09-29 10:48:09 -0500

    added more content into body of email template

--- utils/inviteEmail.js
+++ utils/inviteEmail.js
@@ -21,10 +21,25 @@ async function invitationEmail() {
 	let info = await transporter.sendMail({
 		from: '"Secret Santa App" <secretsanta.partyapp@gmail.com>', // sender address
 		to: `${email}`, // list of receivers
-		subject: "(Organizer) sent you a Secret Santa invitation!", // Subject line
+		subject: `${organizer} sent you a Secret Santa invitation!`, // Subject line
 		text: " ", // plain text body - nothing included here at this time
 		html: `<b>
-        <h1>${organizer} has invited you to the the ${description} Secret Santa Party!</h>
+		<h1>You are invited the ${description} Secret Santa Party!</h>
+		<p>Here Secret Santa Party information.</p>
+		<li>
+			<ul>Date: ${date}</ul>
+			<ul>Time: ${start_time}</ul>
+			<ul>Location: ${location}</ul>
+		</li>
+
+		<p>Please let ${organizer} know if you plan on attending this Secret Santa event by 
+		clicking on the link provided below to RSVP for the event. The link provided below will
+		redirect you to the Secret Santa App platform.</p>
+
+		<li>
+			<ul><a href="https://secret-santa-platform.herokuapp.com/">
+			https://secret-santa-platform.herokuapp.com/</a></ul>
+		</li>
         </b>`, // html body
 	});
 
