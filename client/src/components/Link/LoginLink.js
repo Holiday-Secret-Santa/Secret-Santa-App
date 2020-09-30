ddc58a4ccffdabc44858e566c91e49de94e8165e
Yadira Tamarit <ystamaritq@gmail.com>
2020-09-30 10:16:52 -0500

    tooltip change bg color and placement

--- client/src/components/Link/LoginLink.js
+++ client/src/components/Link/LoginLink.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
+import { Tooltip } from "antd";
+
+const color = "rgba(90, 136, 110, 0.7)";
 
 // added login link using for authenticate the user once enter to the page (could change to button component and keep the auth0 )
 const LoginLink = () => {
 	const { loginWithRedirect } = useAuth0();
 	return (
-		<Link
-			data-testid="login-btn"
-			to=""
-			onClick={() => loginWithRedirect()}
-		></Link>
+		<Tooltip title="Login/Signup" color={color} key={color} placement="left">
+			<Link
+				data-testid="login-btn"
+				to=""
+				onClick={() => loginWithRedirect()}
+			></Link>
+		</Tooltip>
 	);
 };
 
